4c313b79f7375e98400997e67f9a56e7ea940dfc
Hassan Raza <hr.raza04@gmail.com>
2025-06-29 19:54:07 +0530

    baigan

--- src/utils/asyncHandler.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction, RequestHandler } from "express";
-
-export const asyncHandler = (requestHandler:Function)=>{
-    return  function(req:Request, res:Response, next:NextFunction){
-        Promise.resolve(requestHandler(req,res,next))
-        .catch((errr)=> next(errr))
-    }
-}
